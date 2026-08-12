6efbc2d56ff14a85b4ad493a0da439fd1b7ff619
Binkatesh kumar <binkatesh.kumar.cs28@iilm.edu>
2026-08-12 16:14:14 +0530

    Time: 3 ms (89.62%) | Memory: 64.7 MB (86.58%) - LeetSync

--- 3206-find-common-elements-between-two-arrays/find-common-elements-between-two-arrays.cpp
+++ 3206-find-common-elements-between-two-arrays/find-common-elements-between-two-arrays.cpp
@@ -24,3 +24,33 @@ public:
         return ans;
     }
 };
+
+
+
+//map
+
+class solution {
+public:
+    vector<int> findIntersectionValues(vector<int>& nums1, vector<int>& nums2) {
+        map<int, int> mp1, mp2;
+        vector<int> ans(2, 0);
+
+        for(int x : nums1)
+            mp1[x]++;
+
+        for(int x : nums2)
+            mp2[x]++;
+
+        for(int x : nums1) {
+            if(mp2[x] > 0)
+                ans[0]++;
+        }
+
+        for(int x : nums2) {
+            if(mp1[x] > 0)
+                ans[1]++;
+        }
+
+        return ans;
+    }
+};
